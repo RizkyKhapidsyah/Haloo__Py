479408da17820795081a17ef0f863cd8bda85e3a
Rizky Hafitsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-06-23 17:11:56 +0700

    Add files via upload

---
+++ Haloo__Py.py
@@ -0,0 +1,11 @@
+
+print("blablabla")      #String
+print("13+1")           #String
+print(13+1)             #Numerik Penjumlahan
+print(13-1)             #Numerik Pengurangan
+print(13*2)             #Numerik Perkalian
+print(13/2)             #Numerik Pembagian (otomatis menjadi bilangan pecahan)
+print(pow(13, 2))       #Numerik Perpangkatan (cara 1)
+print(13**2)            #Numerik Perpangkatan (cara 2)
+print(13//2)            #Numerik Pembagian (Hasil langsung di bulatkan)
+print(13%2)             #Numerik Modulus
